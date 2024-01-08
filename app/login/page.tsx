9074049f8ce75f88ca2a5f8b63dd25df0e9ea6ec
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-01-08 11:07:13 +0530

    login page created

---
+++ app/login/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+import Loader from "@/components/Loader";
+import axios from "axios";
+import { redirect, useRouter } from "next/navigation";
+import {
+  ChangeEventHandler,
+  MouseEventHandler,
+  useEffect,
+  useState,
+} from "react";
+
+export default function Login() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [token, setToken] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    let token = window.localStorage.getItem("token");
+    if (token) redirect("/posts");
+  }, [router]);
+
+  useEffect(() => {
+    if (token) {
+      window.localStorage.setItem("token", token);
+      redirect("/posts");
+    }
+  }, [token]);
+
+  const onEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setEmail(e.target.value);
+    setError("");
+  };
+  const onPasswordChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPassword(e.target.value);
+    setError("");
+  };
+  const login: MouseEventHandler = (e) => {
+    setLoading(true);
+    axios
+      .post(`${process.env.NEXT_PUBLIC_URL_BACKEND}/signin`, {
+        email,
+        password,
+      })
+      .then((data) => {
+        setToken(data.data.token);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.name === "AxiosError") setError(err.response.data.message);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="flex flex-col h-[calc(100vh-120px)] justify-center items-center w-3/5 text-white mx-auto">
+      <form className="w-full bg-card rounded-lg flex flex-col gap-4 justify-around items-center py-8 ">
+        <input
+          className="bg-divider p-2 font-[#FFF] rounded-lg block w-3/5"
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={onEmailChange}
+        />
+        <input
+          className="bg-divider p-2 font-[#FFF] rounded-lg block w-3/5"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={onPasswordChange}
+        />
+        {error && <p className="text-red p-2">{error}</p>}
+        {loading ? (
+          <Loader />
+        ) : (
+          <button
+            className="bg-green hover:bg-hgreen px-4 py-2 rounded-lg"
+            onClick={login}
+          >
+            Login
+          </button>
+        )}
+      </form>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+"use client";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    let token = window.localStorage.getItem("token");
+    if (token) redirect("/posts");
+    else redirect("/login");
+  }, []);
   return (
     <div className="flex h-[calc(100vh-120px)] justify-center items-center text-3xl">
       {/* <div className="h-2/5"> */}
