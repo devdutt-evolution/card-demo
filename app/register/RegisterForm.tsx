971b0a4bf0241e42a9da1348f256ecdad2b3278c
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-01-17 16:41:57 +0530

    integrated form management lib

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -3,84 +3,96 @@
 import Loader from "@/components/Loader";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type FormBody = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 
 export default function RegisterForm() {
   const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const {
+    register,
+    formState: { errors, isLoading },
+    handleSubmit,
+  } = useForm<FormBody>();
   const [error, setError] = useState("");
 
-  async function register() {
-    try {
-      setLoading(true);
-      let res = await fetch(`${process.env.NEXT_PUBLIC_URL_BACKEND}/register`, {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, username: userName, password }),
-      });
-      setLoading(false);
-      if (res.status == 201) router.push("/login");
-    } catch (err: any) {
-      setLoading(false);
-      setError(err);
+  async function registerUser(data: FormBody) {
+    let res = await fetch(`${process.env.NEXT_PUBLIC_URL_BACKEND}/register`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: data.email,
+        username: data.username,
+        password: data.password,
+      }),
+    });
+    if (res.status == 201) router.push("/login");
+    else {
+      let data = await res.json();
+      setError(data?.message || "Registration Failed");
     }
   }
 
   return (
-    <form className="w-full bg-card rounded-lg flex flex-col gap-4 justify-around items-center py-8 ">
+    <form
+      className="w-full bg-card rounded-lg flex flex-col gap-4 justify-center items-center p-5"
+      noValidate
+      onSubmit={handleSubmit(registerUser)}
+    >
       <input
-        className="outline-none focus:outline-green bg-divider p-2 font-[#FFF] rounded-lg block w-3/5"
+        className="outline-none focus:outline-green bg-divider px-3 p-2 font-[#FFF] rounded-lg w-full"
         type="text"
         placeholder="username"
-        value={userName}
-        onChange={(e) => {
-          setUserName(e.target.value);
-          setError("");
-        }}
+        {...register("username", { required: "Username is required" })}
       />
+      {errors.username && (
+        <p className="text-red p-2">{errors.username.message}</p>
+      )}
       <input
-        className="outline-none focus:outline-green bg-divider p-2 font-[#FFF] rounded-lg block w-3/5"
+        className="outline-none focus:outline-green bg-divider px-3 p-2 font-[#FFF] rounded-lg w-full"
         type="text"
         placeholder="email"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-          setError("");
-        }}
+        {...register("email", { required: "Email is required" })}
       />
+      {errors.email && <p className="text-red p-2">{errors.email.message}</p>}
       <input
-        className="outline-none focus:outline-green bg-divider p-2 font-[#FFF] rounded-lg block w-3/5"
+        className="outline-none focus:outline-green bg-divider px-3 p-2 font-[#FFF] rounded-lg w-full"
         type="password"
         placeholder="password"
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-          setError("");
-        }}
+        {...register("password", { required: "Password is required" })}
       />
+      {errors.password && (
+        <p className="text-red p-2">{errors.password.message}</p>
+      )}
       <input
-        className="outline-none focus:outline-green bg-divider p-2 font-[#FFF] rounded-lg block w-3/5"
+        className="outline-none focus:outline-green bg-divider px-3 p-2 font-[#FFF] rounded-lg w-full"
         type="text"
         placeholder="confirm password"
-        value={passwordConfirm}
-        onChange={(e) => {
-          setPasswordConfirm(e.target.value);
-          setError("");
-        }}
+        {...register("confirmPassword", {
+          required: "Confirm the password",
+          validate: {
+            checkPassword: (value, values) => {
+              if (value == values.password) return value;
+              else throw new Error("Passwords does not match");
+            },
+          },
+        })}
       />
-      {passwordConfirm != "" && passwordConfirm != password && (
-        <p className="text-red p-2">Passwords does not match.</p>
+      {errors.confirmPassword && (
+        <p className="text-red p-2">{errors.confirmPassword.message}</p>
       )}
       {error && <p className="text-red p-2">{error}</p>}
-      {loading ? (
+      {isLoading ? (
         <Loader />
       ) : (
         <button
-          className="bg-green hover:bg-hgreen px-4 py-2 rounded-lg"
-          onClick={(e) => register()}
+          className="bg-green hover:bg-opacity-20 px-4 py-2 rounded-lg w-full"
+          type="submit"
         >
           Register
         </button>
