f877cc5e2cb20a1a793fe065a2133aaa21e0f795
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-01-19 14:43:04 +0530

    error

--- app/test/offers/page.tsx
+++ app/test/offers/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 export default function Offers() {
   return (
-      <div className="w-full flex justify-center items-center h-[70vh]">
-        <main className="w-full bg-green text-[#fff] text-5xl">
-          <h2>Offers Page</h2>
-          <p>50% discount on T shirts</p>
-        </main>
-      </div>
+    <div className="w-full flex justify-center items-center h-[70vh]">
+      <main className="w-full bg-green text-[#fff] text-5xl">
+        <h2>Offers Page</h2>
+        <p>50% discount on T shirts</p>
+      </main>
+    </div>
   );
 }
