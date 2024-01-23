520683fef77bbcb4247e350b7f6fb183cc70d77c
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-01-23 13:53:07 +0530

    notifications

--- app/_components/Notifications.tsx
+++ app/_components/Notifications.tsx
@@ -1,22 +1,35 @@
 import { options } from "@/utils/options";
 import { getServerSession } from "next-auth";
-import NotificationIcon from "@/components/icons/NotificationIcon";
+import ContentElement from "./Tips";
+
+export type Notification = {
+  _id: string;
+  title: string;
+  description: string;
+};
+
+async function getNotificationsCount(token: string) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_URL_BACKEND}/notifications`,
+    {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (res.status != 200) throw new Error("failed to fetch notifications");
+
+  const data: { notifications: Notification[] } = await res.json();
+
+  return data.notifications;
+}
 
 export default async function Notifications() {
   const authData = await getServerSession(options);
   const tokener: any = authData?.user;
 
-  // const notificationsCount = await getNotificationsCount(tokener.token);
-  const notificationsCount = 8;
+  const notifications = await getNotificationsCount(tokener.token);
 
-  return (
-    <div className="relative w-max flex justify-center items-center">
-      <NotificationIcon />
-      {notificationsCount > 0 && (
-        <p className="absolute top-[4px] right-0 bg-red rounded-full text-xs w-4 h-4 text-center">
-          {notificationsCount > 9 ? "9+" : notificationsCount}
-        </p>
-      )}
-    </div>
-  );
+  return <ContentElement notifications={notifications} />;
 }
---
+++ app/_components/Tips.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import NotificationIcon from "@/components/icons/NotificationIcon";
+import Tippy from "@tippyjs/react";
+import { ReactNode } from "react";
+import type { Notification } from "./Notifications";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+function createJsx(notifications: Notification[]): ReactNode {
+  return notifications.map((notificationObj) => (
+    <li key={notificationObj._id} className="cursor-default w-full ">
+      <h4 className="text-sm font-bold text-green">{notificationObj.title}</h4>
+      <p className="text-sm">{notificationObj.description}</p>
+    </li>
+  ));
+}
+
+export default function ContentElement({
+  notifications,
+}: {
+  notifications: Notification[];
+}) {
+  const { data: authData } = useSession({ required: true });
+  const router = useRouter();
+  const tokener: any = authData?.user;
+  const notificationsCount = notifications.length;
+
+  async function markSeen() {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_BACKEND}/notifications`,
+      {
+        method: "PUT",
+        headers: {
+          authorization: `Bearer ${tokener.token}`,
+        },
+      }
+    );
+
+    if (res.status != 200) throw new Error("failed to mark as read");
+    router.refresh();
+  }
+
+  return (
+    <Tippy
+      placement="bottom-end"
+      content={
+        <div className="bg-card rounded-lg p-2 w-80 max-h-80 shadow-[0_0_30px_2px_#00875F] overflow-y-auto">
+          <div className="flex justify-between items-center text-sm text-place border-b-2 border-b-divider">
+            <p className="w-max">LATEST</p>
+            <p className="cursor-pointer w-max" onClick={(e) => markSeen()}>
+              Mark all Seen
+            </p>
+          </div>
+          {notificationsCount > 0 ? (
+              <ul className="flex flex-col gap-2 w-full py-2">
+                {createJsx(notifications)}
+              </ul>
+          ) : (
+            <p className="text-sm pt-2">No Recent Notifications</p>
+          )}
+        </div>
+      }
+      interactive={true}
+    >
+      <div className="relative w-max flex justify-center items-center cursor-pointer">
+        <NotificationIcon />
+        {notificationsCount > 0 && (
+          <p className="absolute top-[4px] right-0 bg-red rounded-full text-xs w-4 h-4 text-center">
+            {notificationsCount > 9 ? "9+" : notificationsCount}
+          </p>
+        )}
+      </div>
+    </Tippy>
+  );
+}
