0866155f6cb7b080034fef0538305cdf3edf0ed2
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-04-05 16:00:17 +0530

    edited tag on posts

--- app/posts/_components/Posts.tsx
+++ app/posts/_components/Posts.tsx
@@ -62,7 +62,7 @@ export default function InfinitePosts({ posts }: { posts: Post[] }) {
   }
 
   return (
-    <div className="flex flex-col w-full mx-auto gap-2">
+    <div className='flex flex-col w-full mx-auto gap-2'>
       {data?.length > 0 &&
         data?.map((post) => {
           let diff = post.publishAt;
@@ -72,54 +72,59 @@ export default function InfinitePosts({ posts }: { posts: Post[] }) {
           return (
             <div
               key={post.body}
-              className="bg-card hover:border-2 hover:border-green hover:shadow-sm hover:shadow-green px-8 py-6 border-2 border-card rounded-lg"
+              className='bg-card hover:border-2 hover:border-green hover:shadow-sm hover:shadow-green px-8 py-6 border-2 border-card rounded-lg'
             >
-              <Link className="w-fit" href={`/user/${post.userId}`}>
-                <div className="flex justify-between">
-                  <h2 className="text-green text-2xl">{post.user?.username}</h2>
+              <Link className='w-fit' href={`/user/${post.userId}`}>
+                <div className='flex justify-between'>
+                  <h2 className='text-green text-2xl'>{post.user?.username}</h2>
                   {luxonDate && <h3>{luxonDate?.toRelative()}</h3>}
                 </div>
-                <h3 className="mb-4 text-sm">{post.user?.name}</h3>
+                <div className='flex justify-between items-start'>
+                  <h3 className='mb-4 text-sm'>{post.user?.name}</h3>
+                  {post.isEdited && (
+                    <p className='text-white/60 text-sm'>Edited</p>
+                  )}
+                </div>
               </Link>
               <div>
                 <Link href={`/post/${post._id}`}>
-                  <h3 className="pb-4 text-xl hover:text-green">
+                  <h3 className='pb-4 text-xl hover:text-green'>
                     {post.title}
                   </h3>
                 </Link>
                 <div
-                  className="ProseMirror nono pb-4"
+                  className='ProseMirror nono pb-4'
                   dangerouslySetInnerHTML={{ __html: post.body }}
                 />
               </div>
-              <div className="w-min flex gap-4 p-1 bg-black rounded-full">
+              <div className='w-min flex gap-4 p-1 bg-black rounded-full'>
                 <CustomLike
                   reactionType={
                     post?.userLike?.reactionType || REACTIONS.UNLIKE
                   }
                   likeCount={post.numberOfLikes}
                   postId={post._id}
-                  varient="post"
-                  commentId=""
+                  varient='post'
+                  commentId=''
                 />
                 <Link
-                  className="w-min hover:bg-card flex gap-3 p-1 px-2 rounded-full"
+                  className='w-min hover:bg-card flex gap-3 p-1 px-2 rounded-full'
                   href={`/post/${post._id}`}
                 >
                   <CommentIcon />
-                  <p className="text-md">{post.commentCount}</p>
+                  <p className='text-md'>{post.commentCount}</p>
                 </Link>
               </div>
             </div>
           );
         })}
       {loading && (
-        <div className="h-[200px] flex justify-center items-center w-full">
+        <div className='h-[200px] flex justify-center items-center w-full'>
           <Loader />
         </div>
       )}
       {data.length == 0 && (
-        <div className="bg-card px-3 py-4 border-2 border-black rounded-lg">
+        <div className='bg-card px-3 py-4 border-2 border-black rounded-lg'>
           No Posts
         </div>
       )}
