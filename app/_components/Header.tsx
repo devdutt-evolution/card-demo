afce4d6ba42e4e56b31690d1544898936a0f667c
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-04-08 16:53:23 +0530

    moved header in components

---
+++ app/_components/Header.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import Notifications from "./Notifications";
+import AuthButton from "@/components/AuthButton";
+import { getServerSession } from "next-auth";
+import { options } from "@/utils/options";
+import Avatar from "./Avatar";
+
+export default async function Header() {
+  const authData = await getServerSession(options);
+
+  return (
+    <header className='grid grid-cols-3 w-full h-[100px] bg-card sticky top-0'>
+      <Link
+        className='flex gap-2 col-start-2 justify-center items-center'
+        href='/'
+      >
+        <Image src='/logo.svg' alt='logo' width='65' height='61' />
+        <div className='text-4xl flex justify-center items-center'>
+          <p>કથાnak</p>
+        </div>
+      </Link>
+      <div className='flex justify-end gap-4 items-center pr-4'>
+        {authData?.user?.admin && (
+          <Link
+            href='/reported'
+            className='px-4 py-2 text-white tracking-wide rounded-lg bg-green'
+          >
+            Reports
+          </Link>
+        )}
+        {authData?.user && <Notifications />}
+        <AuthButton />
+        {authData?.user && (
+          <Avatar
+            username={authData.user?.name}
+            profile={authData.user?.picture}
+          />
+        )}
+      </div>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
-import Link from "next/link";
-import AuthButton from "@/components/AuthButton";
 import NextProvider from "./_components/NextProvider";
-import Notifications from "./_components/Notifications";
-import { getServerSession } from "next-auth";
-import { options } from "@/utils/options";
-import Avatar from "./_components/Avatar";
 import type { PropsWithChildren } from "react";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,30 +13,11 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: PropsWithChildren) {
-  const authData = await getServerSession(options);
-
   return (
     <html lang='en'>
       <body className={inter.className}>
         <NextProvider>
-          <header className='flex w-full h-[100px] bg-card justify-center items-center sticky top-0 z-[1]'>
-            <Link className='flex gap-2' href='/'>
-              <Image src='/logo.svg' alt='logo' width='65' height='61' />
-              <div className='text-4xl flex justify-center items-center'>
-                <p>કથાnak</p>
-              </div>
-            </Link>
-            <div className='absolute top-50% mr-4 right-0 w-max flex gap-4'>
-              {authData?.user && <Notifications />}
-              <AuthButton />
-              {authData?.user && (
-                <Avatar
-                  username={authData.user?.name}
-                  profile={authData.user?.picture}
-                />
-              )}
-            </div>
-          </header>
+          <Header />
           {children}
           <div id='modal-root'></div>
         </NextProvider>
